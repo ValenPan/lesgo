b383927df237a465b95d242b69842a29ad03cceb
ValenPan <valenpunki@gmail.com>
2016-02-06 20:21:57 +0100

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  get '/aboutus' => 'pages#about' #esto crea un aboutus path
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
